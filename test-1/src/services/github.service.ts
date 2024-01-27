38312f715c5a6dab56a05f44887cc718dc11a26a
Edward Brito Diaz <37183622+Edwardb11@users.noreply.github.com>
2024-01-27 08:08:52 -0400

    fix: Refactor getTopRepositories function to use TypeScript interfaces

--- test-1/src/services/github.service.ts
+++ test-1/src/services/github.service.ts
@@ -1,14 +1,26 @@
 import axios, { AxiosResponse, AxiosError } from "axios";
 
-export async function getTopRepositories(username: string): Promise<any[]> {
+interface Repository {
+  stargazers_count: number;
+}
+
+export async function getTopRepositories(
+  username: string
+): Promise<Repository[]> {
   try {
     const githubApiUrl = `https://api.github.com/users/${username}/repos`;
     const config = { headers: { "User-Agent": "github-api-app" } };
 
-    const response: AxiosResponse = await axios.get(githubApiUrl, config);
+    const response: AxiosResponse<Repository[]> = await axios.get(
+      githubApiUrl,
+      config
+    );
 
     return response.data
-      .sort((a: any, b: any) => b.stargazers_count - a.stargazers_count)
+      .sort(
+        (a: Repository, b: Repository) =>
+          b.stargazers_count - a.stargazers_count
+      )
       .slice(0, 10);
   } catch (error) {
     throw handleError(error as AxiosError);
